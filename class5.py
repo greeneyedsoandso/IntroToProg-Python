8e26326091a51b8bb78ad67d888a9051395efcf0
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-02-18 19:45:50 -0800

    removing stuff

--- class5.py
+++ class5.py
@@ -17,3 +17,23 @@ for row in objFile:
     lstTable.append(dicRow)
     print(lstTable, '<< List with Dictionary objects')
 objFile.close()
+
+while(True):
+    strItem = input('Item: ')
+    strValue = input('Value: ')
+    lstTable.append({'item': strItem, 'value': strValue})
+    strChoice = input("Exit? ('y/n'): ")
+    if strChoice.lower() == 'y':
+        break
+    print(lstTable, '<< List with Dictionary objects')
+
+while(True):
+    strItem = input('item to remove: ')
+    for row in lstTable:
+        if row['item'].lower() != strItem.lower():
+            print('row not found')
+            print(lstTable, '<< List with Dictionary objects')
+        else:
+            lstTable.remove(row)
+            print('row removed')
+            print(lstTable, '<< List with Dictionary objects')
